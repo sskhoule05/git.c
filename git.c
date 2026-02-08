480ca210e1c02d40f04756e5138616345749b951
sskhoule05 <sskhoue05@gmaim.com>
2026-02-08 22:33:46 +0000

    update2

--- git.c
+++ git.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(){
-
+    int i;
+    scanf("%d",&i);
+    if (i>0){
+        printf("nombre positif");
+    }
+    else {
+        printf("nombre negatif");
+    }
 
 
 return 0;
